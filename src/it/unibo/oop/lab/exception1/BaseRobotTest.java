8c756f26625f5e6152e8e78f650c0178f2b9abbf
Giacomo Sirri <sirrigiacomo9@gmail.com>
2021-11-05 19:25:14 +0100

    Modify move() to throw exception, modify Robot and BaseRobotTest accordingly

--- src/it/unibo/oop/lab/exception1/BaseRobotTest.java
+++ src/it/unibo/oop/lab/exception1/BaseRobotTest.java
@@ -3,6 +3,8 @@ package it.unibo.oop.lab.exception1;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -34,23 +36,34 @@ public final class BaseRobotTest {
          * 2) Move the robot right until it touches the world limit
          */
         for (int i = 0; i < RobotEnvironment.WORLD_X_UPPER_LIMIT; i++) {
-            // check if position if coherent
+            // check if position is coherent
             assertTrue("[CHECKING MOVING RIGHT]", r1.moveRight());
         }
         // reached the right limit of the world
-        assertFalse("[CHECKING MOVING RIGHT]", r1.moveRight());
+        try {
+        	r1.moveRight();
+        	fail();
+        } catch (PositionOutOfBoundException e) {
+        	assertNotNull(e.getMessage());
+        }
         // checking positions x=50; y=0
         assertEquals("[MOVING RIGHT ROBOT POS X]", RobotEnvironment.WORLD_X_UPPER_LIMIT, r1.getEnvironment().getCurrPosX());
         assertEquals("[MOVING RIGHT ROBOT POS Y]", 0, r1.getEnvironment().getCurrPosY());
+
         /*
-         * 2) Move to the top until it reaches the upper right conrner of the world
+         * 3) Move to the top until it reaches the upper right corner of the world
          */
         for (int i = 0; i < RobotEnvironment.WORLD_Y_UPPER_LIMIT; i++) {
-            // check if position if coherent
+            // check if position is coherent
             assertTrue("[CHECKING MOVING UP]", r1.moveUp());
         }
         // reached the upper limit of the world
-        assertFalse("[CHECKING MOVING UP]", r1.moveUp());
+        try {
+        	r1.moveUp();
+        	fail();
+        } catch (PositionOutOfBoundException e) {
+        	assertNotNull(e.getMessage());
+        }
         // checking positions x=50; y=80
         assertEquals("[MOVING RIGHT ROBOT POS X]", RobotEnvironment.WORLD_X_UPPER_LIMIT, r1.getEnvironment().getCurrPosX());
         assertEquals("[MOVING RIGHT ROBOT POS Y]", RobotEnvironment.WORLD_Y_UPPER_LIMIT, r1.getEnvironment().getCurrPosY());
--- src/it/unibo/oop/lab/exception1/Robot.java
+++ src/it/unibo/oop/lab/exception1/Robot.java
@@ -86,13 +86,12 @@ public class Robot {
     private boolean moveToPosition(final int newX, final int newY) {
         boolean returnValue = true;
         if (this.isBatteryEnoughToMove()) {
-            if (this.environment.move(newX, newY)) {
+            try { 
+            	this.environment.move(newX, newY);
                 this.consumeBatteryForMovement();
                 this.log("Moved to position(" + newX + "," + newY + ").");
-            } else {
-                this.log("Can not move to (" + newX + "," + newY
-                        + ") the robot is touching at least one world boundary");
-                returnValue = false;
+            } catch (PositionOutOfBoundException e) {
+                throw e;
             }
         } else {
             this.log("Can not move to position(" + newX + "," + newY + "). Not enough battery.");
